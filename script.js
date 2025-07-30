ad6d199a38b542c5daf6cb05d583e12a3f2cc1df
BenScripts <145988009+bennah995@users.noreply.github.com>
2025-07-30 12:27:53 +0300

    Wrpped my content in DOMContentLoaded

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
-const form = document.getElementById('registration-form');
-const feedbackDiv = document.getElementById('form-feedback');
+document.addEventListener('DOMContentLoaded', function(){
+  const form = document.getElementById('registration-form');
+  const feedbackDiv = document.getElementById('form-feedback');
 
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
 
-  //For each, retrieve the .value property and apply the .trim() method to remove any leading or trailing whitespace. Store these trimmed values in constants named after each input field.
+    //For each, retrieve the .value property and apply the .trim() method to remove any leading or trailing whitespace. Store these trimmed values in constants named after each input field.
 
-  const usernameInput = document.getElementById('username').value.trim();
-  const emailInput = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
+    const usernameInput = document.getElementById('username').value.trim();
+    const emailInput = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
-  let isValid = true;
-  const messages = [];
+    let isValid = true;
+    const messages = [];
 
-  //username validation
-  if (usernameInput.length < 3){
-    isValid = false;
-    messages.push('Username must be at least 3 characters long.');
-  }
+    //username validation
+    if (usernameInput.length < 3){
+      isValid = false;
+      messages.push('Username must be at least 3 characters long.');
+    }
 
-  //email validation
-  if(!emailInput.includes('@') || !emailInput.includes('.')){
-    isValid = false;
-    messages.push("Email must include '@' and '.'");
-  }
+    //email validation
+    if(!emailInput.includes('@') || !emailInput.includes('.')){
+      isValid = false;
+      messages.push("Email must include '@' and '.'");
+    }
 
-  //password validation
-  if(password.length < 8){
-    isValid = false;
-    messages.push('Password must be atleast 8 characters long');
-  }
+    //password validation
+    if(password.length < 8){
+      isValid = false;
+      messages.push('Password must be atleast 8 characters long');
+    }
 
-  //displaing feedback
-  feedbackDiv.style.display = "block";
+    //displaing feedback
+    feedbackDiv.style.display = "block";
 
 
-  if(isValid){
-    feedbackDiv.textContent = 'registration successful!';
-    feedbackDiv.style.color = '#28a745'
-  } else {
-    feedbackDiv.innerHTML = messages.join("<br>");
-    feedbackDiv.style.color = '#dc3545';
-  }
+    if(isValid){
+      feedbackDiv.textContent = 'registration successful!';
+      feedbackDiv.style.color = '#28a745'
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+      feedbackDiv.style.color = '#dc3545';
+    }
+  });
 });
